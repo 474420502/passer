65b9d069ae718b480964c5f0c2d087f44a0dd612
eson <9673575+githubcontent@user.noreply.gitee.com>
2023-08-02 16:54:45 +0800

    添加注释

--- passer.go
+++ passer.go
@@ -1,3 +1,4 @@
+// passer包实现通用的函数执行和序列化
 package passer
 
 import (
@@ -9,47 +10,64 @@ import (
 	"sync"
 )
 
+// 执行结果
 type executeResult[RESULT any] struct {
 	result *RESULT
 	err    error
 }
 
+// 分隔符
 var sep = []byte("!?@#")
 
+// 注册函数类型
 type Dofunc[RESULT any] func(cxt context.Context, obj any) (RESULT, error)
 
+// 已注册函数信息
 type passerType[RESULT any] struct {
 	SType  reflect.Type
 	Dofunc Dofunc[RESULT]
 }
 
+// Passer 管理注册信息
 type Passer[RESULT any] struct {
-	mu             sync.Mutex
+	// 锁保护共享数据
+	mu sync.Mutex
+
+	// 注册表
 	registedObject map[string]*passerType[RESULT]
 }
 
+// 创建
 func NewPasser[RESULT any]() *Passer[RESULT] {
 	return &Passer[RESULT]{
 		registedObject: make(map[string]*passerType[RESULT]),
 	}
 }
 
+// 注册函数
 func (p *Passer[RESULT]) RegisterPasser(t any, do Dofunc[RESULT]) {
+
+	// 加锁
 	p.mu.Lock()
 	defer p.mu.Unlock()
+
+	// 获取类型
 	stype := reflect.TypeOf(t)
 
+	// 保存注册信息
 	p.registedObject[stype.String()] = &passerType[RESULT]{
 		SType:  stype,
 		Dofunc: do,
 	}
 }
 
+// 序列化
 func (p *Passer[RESULT]) PackToBytes(obj any) ([]byte, error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 	stype := reflect.TypeOf(obj)
 	stypestr := stype.String()
+	// 检查注册
 	if _, ok := p.registedObject[stypestr]; !ok {
 		return nil, fmt.Errorf("struct is not registed: %v", obj)
 	}
@@ -64,13 +82,16 @@ func (p *Passer[RESULT]) PackToBytes(obj any) ([]byte, error) {
 	return buf.Bytes(), err
 }
 
+// 执行
 func (p *Passer[RESULT]) ExecuteWithBytes(ctx context.Context, data []byte) (RESULT, error) {
 	var result RESULT
 	var err error
 
+	// 找传递数据的类型
 	idx := bytes.Index(data, sep)
 
 	p.mu.Lock()
+	// 找到对应类型的注册函数
 	passer, ok := p.registedObject[string(data[:idx])]
 	if !ok {
 		p.mu.Unlock()
